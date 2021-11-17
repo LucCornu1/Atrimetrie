bb03e7d2112ebeaf6704c61791d41c656ad6f7a6
Luc Cornu <luccornu@hotmail.fr>
2021-11-17 14:15:06 +0100

    GameMode multijoueur

--- Source/Atrimetrie/MultiplayerGameMode.cpp
+++ Source/Atrimetrie/MultiplayerGameMode.cpp
@@ -1,37 +1,42 @@
 // CORNU Luc
 
-/*#include "GameFramework/DefaultPawn.h"
+#include "GameFramework/DefaultPawn.h"
 #include "GameFramework/SpectatorPawn.h"
 #include "GameFramework/HUD.h"
 #include "GameFramework/PlayerState.h"
 #include "GameFramework/GameSession.h"
 #include "GameFramework/PlayerStart.h"
-#include "GameFramework/WorldSettings.h"*/
+#include "GameFramework/WorldSettings.h"
 #include "MultiplayerGameMode.h"
 
 AMultiplayerGameMode::AMultiplayerGameMode()
 {
 	bNetLoadOnClient = false;
 	bPauseable = false;
-	bStartPlayersAsSpectators = true;
+	bStartPlayersAsSpectators = false;
 
-	/*DefaultPawnClass = ADefaultPawn::StaticClass();
+	DefaultPawnClass = ADefaultPawn::StaticClass();
 	PlayerControllerClass = APlayerController::StaticClass();
 	PlayerStateClass = APlayerState::StaticClass();
-	// GameStateClass = AGameStateBase::StaticClass();
+//	GameStateClass = AGameStateBase::StaticClass();
 	HUDClass = AHUD::StaticClass();
 	GameSessionClass = AGameSession::StaticClass();
 	SpectatorClass = ASpectatorPawn::StaticClass();
 	ReplaySpectatorPlayerControllerClass = APlayerController::StaticClass();
-	ServerStatReplicatorClass = AServerStatReplicator::StaticClass();*/
+	ServerStatReplicatorClass = AServerStatReplicator::StaticClass();
+
 }
 
 void AMultiplayerGameMode::BeginPlay()
 {
+	Super::BeginPlay();
+
 }
 
 void AMultiplayerGameMode::Tick(float DeltaTime)
 {
+	Super::Tick(DeltaTime);
+
 }
 
 
